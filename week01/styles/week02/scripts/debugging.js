5e36dfd5626979a924712c7291c2ed0ef8384cd2
Brian Timba <bryantymbr@gmail.com>
2025-07-22 00:06:16 +0200

    week2

---
+++ week01/styles/week02/scripts/debugging.js
@@ -0,0 +1,15 @@
+radiusOutput.textContent = radius;
+areaOutput.textContent = area;
+
+let area = 0;
+const PI = 3.14159;
+
+const radius = 10;
+area = PI * radius * radius;
+radiusOutput = radius;
+areaOutput = area;
+
+radius = 20;
+area = PI * radius * radius;
+radiusOutput = radius;
+areaOutput = area;
